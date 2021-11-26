e979ae70a92935041d8d71c604fb2ff93459acda
IgelCode <tony.angermann@gmail.com>
2021-11-26 22:06:02 +0100

    Finished...maybe?

--- game.js
+++ game.js
@@ -1,34 +1,54 @@
-
-function random_item(items)
-{
-  
-return items[Math.floor(Math.random()*items.length)];
-     
-}
-
-const items = [254, 45, 212, 365, 2543];
-console.log(random_item(items));
-
-
-
-
-
-
-
-
-
-
-
-
-function computerPlay(play) {
-    play[Math.floor(Math.random()*computerPlay.length)];
-    return;
-}
 const play = ["rock", "paper", "scissors"];
-console.log(computerPlay(play));
 
+game();
 
+function game(){
+    for (let i=0; i < 5; i++) {
 
+    const computerSelection = computerPlay(play);
+    const playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
 
+    function computerPlay(play) {
+    return play[Math.floor(Math.random()*play.length)];
+    }
 
+    function playRound() {
+       
+    if (playerSelection == computerSelection) {
+        console.log("Its a tie!");
+    }
+    else if ((playerSelection == "rock") && (computerSelection == "paper")) {
+        console.log("Paper beats Rock, you lost!");
+        
+    }
+    else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
+        console.log("Scissors beats Paper, you lost!");
+        
+    }
+    else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
+        console.log("Rock beats Scissors, you lost!");
+        
+    }
+    else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
+        console.log("Scissors beats Paper, you won!");
+        
+    }
+    else if ((playerSelection == "paper") && (computerSelection == "rock")) {
+        console.log("Paper beats Rock, you won!");
+        
+    }
+    else if  ((playerSelection == "rock") && (computerSelection == "scissors")) {
+        console.log("Rock beats Scissors, you won!");
+        
+    }
+    else {
+        console.log("Invalid input.")
+        
+    }
+    }
+    console.log(playRound());
+    
 
+    }
+    
+}
--- test.js
+++ test.js
@@ -19,15 +19,15 @@ function playRound(playerSelection, computerSelection) {
   function computerPlay() {
     computerPlay = Math.floor(Math.random()*10);
     
-if (computerPlay <= 3) {
+if (computerPlay <= 3.3) {
     computerPlay = "rock";
     return;
 }
-else if (computerPlay > 3 && computerPlay <= 6) {
+else if (computerPlay > 3.3 && computerPlay <= 6.6) {
     computerPlay = "paper";
     return;
 }
-else if (computerPlay > 6) {
+else if (computerPlay > 6.6) {
     computerPlay = "scissors";
     return;
 }
@@ -35,3 +35,35 @@ else if (computerPlay > 6) {
 computerPlay();
 console.log(computerPlay);
 
+
+
+
+
+
+computerSelection = computerPlay(play);
+
+if (playerSelection == computerSelection) {
+    console.log("Its a tie!");
+}
+else if ((playerSelection = "rock") && (computerSelection = "paper")) {
+    console.log("Paper beats Rock, you lost!");
+}
+else if ((playerSelection = "paper") && (computerSelection = "scissors")) {
+    console.log("Scissors beats Paper, you lost!");
+}
+else if ((playerSelection = "scissors") && (computerSelection = "rock")) {
+    console.log("Rock beats Scissors, you lost!");
+}
+else if ((playerSelection = "scissors") && (computerSelection = "paper")) {
+    console.log("Scissors beats Paper, you won!");
+}
+else if ((playerSelection = "paper") && (computerSelection = "rock")) {
+    console.log("Paper beats Rock, you won!");
+}
+else if  ((playerSelection = "rock") && (computerSelection = "scissors")) {
+    console.log("Rock beats Scissors, you won!");
+}
+else {
+    console.log("Please input rock, paper or scissors.")
+}
+}
