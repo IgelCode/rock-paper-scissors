3d96171afb5e5f2625bfa6140c903c36ef8c47b0
IgelCode <tony.angermann@gmail.com>
2021-12-06 23:56:35 +0100

    added a bunch of css+splashscreen

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('.buttons');
+const buttons = document.querySelectorAll('.button');
 const play = ['rock', 'paper', 'scissors'];
 const rockBtn = document.querySelector('#rock');
 const paperBtn = document.querySelector('#paper');
@@ -9,8 +9,16 @@ const PCScore = document.querySelector('#pc-score');
 let computerSelection = computerPlay(play);
 let pc = 0;
 let player = 0;
-
 let playerSelection;
+
+const enterLink = document.querySelector('.enter_link');
+const splashScreen = document.querySelector('.splashscreen');
+
+enterLink.addEventListener('click', () => {
+    splashScreen.style.transition = '1.5s';
+    splashScreen.style.opacity = '0';
+    splashScreen.style.visibility = 'hidden';
+});
 
 buttons.forEach((button) => {
     button.addEventListener('click', getResult);
@@ -42,34 +50,36 @@ function winner() {
         paperBtn.setAttribute('disabled', 'disabled');
         scissorsBtn.setAttribute('disabled', 'disabled');
         if (player === 5) {
-            result.textContent = 'You have won!';
+            result.textContent = 'Good job, you have won! Christmas is saved :)';
+            result.setAttribute('style', 'color:red; font-size:30px');
         } else if (pc === 5) {
-            result.textContent = 'PC has won!';
+            result.textContent = 'Grinch won! Grinch stole Christmas :(';
+            result.setAttribute('style', 'color:red; font-size:30px');
         }
     }
 }
 
 function playRound() {
     if (playerSelection == computerSelection) {
-        return 'Its a tie!';
+        return 'Its a tie! Try harder!';
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
         pc += 1;
-        return 'Paper beats Rock, you lost!';
+        return 'Grinch picked paper, you lost!';
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
         pc += 1;
-        return 'Scissors beats Paper, you lost!';
+        return 'Grinch picked scissors, you lost!';
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         pc += 1;
-        return 'Rock beats Scissors, you lost!';
+        return 'Grinch picked rock, you lost!';
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
         player += 1;
-        return 'Scissors beats Paper, you won!';
+        return 'Grinch picked paper, you won!';
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
         player += 1;
-        return 'Paper beats Rock, you won!';
+        return 'Grinch picked rock, you won!';
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
         player += 1;
-        return 'Rock beats Scissors, you won!';
+        return 'Grinch picked scissors, you won!';
     } else {
         return 'Invalid input.';
     }
